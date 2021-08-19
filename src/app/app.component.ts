fe766cbcfe4ff4a7fd7d72f3922fa33ad0098107
Victor <villazante.bernabe.victor@gmail.com>
2021-08-19 00:44:09 -0400

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { throwError } from 'rxjs';
+//import { retry } from 'rxjs/operators';
+//import { catchError } from 'rxjs/operators';
+import { Meme } from "./meme";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'proyectoAngular';
+  m:Meme[]=[];
+  constructor(private http:HttpClient){}
+  cargarMemes():void{
+    console.log("cargar memes");  
+    //Es any con minuscula no con mayuscula Any sino no define un tipo de variable dinamica
+    this.http.get<any>("https://api.imgflip.com/get_memes").subscribe(response=>{
+      this.m=response.data.memes;
+      console.log(this.m);
+    });
+  }
 }
---
+++ src/app/meme.ts
@@ -0,0 +1,12 @@
+export class Meme{
+    id:number=0;
+    name:string="";
+    url:string="";
+    width:number=0;
+    height:number=0;
+    box_count:number=0;
+    data:String[]=[];
+    constructor(){
+
+    }
+}
